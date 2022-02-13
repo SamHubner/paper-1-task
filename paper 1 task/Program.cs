dba5ea2b6a4950b45083d6ee6b5c029da070f622
Sam Hubner <K038639@eltham-college.org.uk>
2022-02-13 17:32:07 +0000

    Add project files.

---
+++ paper 1 task/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace paper_1_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentCount = 0;
+            int maxCount;
+            int[] values = new int[10];
+            int temp = 0;
+
+            for(int x = 0; x<values.Length; x = x + 1)
+            {
+                values[x] = 0;
+            }
+            
+            Console.WriteLine("How many digits?");
+            maxCount = Convert.ToInt32(Console.ReadLine());
+            while (currentCount < maxCount)
+            {
+                int insertedValue = Convert.ToInt32(Console.ReadLine());
+                values[insertedValue] = values[insertedValue] + 1;
+                currentCount = currentCount + 1;
+            }
+
+            
+
+            for (int write = 0; write < values.Length; write++)
+            {
+                for (int sort = 0; sort < values.Length - 1; sort++)
+                {
+                    if (values[sort] > values[sort + 1])
+                    {
+                        temp = values[sort + 1];
+                        values[sort + 1] = values[sort];
+                        values[sort] = temp;
+                    }
+                }
+                
+            }
+            bool same = true;
+            int y = 9;
+            while(same == true)
+            {
+                if (values[y] == values[y - 1]) same = true;
+                else same = false;
+                Console.WriteLine("{0} ", values[y]);
+            }
+
+        }
+    }
+}
